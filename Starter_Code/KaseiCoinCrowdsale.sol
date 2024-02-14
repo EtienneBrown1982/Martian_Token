2af4aa128b35c0ab99c77ce750a43c1854cb95a1
EtienneBrown1982 <etiennebrown@gmail.com>
2024-02-13 18:38:22 -0600

    updated code and readme. files are ready for submission.

--- Starter_Code/KaseiCoinCrowdsale.sol
+++ Starter_Code/KaseiCoinCrowdsale.sol
@@ -1,101 +1,49 @@
+pragma solidity ^0.5.5;
 
-File explorer
+import "./KaseiCoin.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+// Added portion to attempt capped crowdsale
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+// Added portion to attempt timed crowdsale
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
 
-1
-
-    1
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale {
+    
     constructor(
-0
-listen on all transactions
- Welcome to Remix 0.42.0 
-
-Your files are stored in indexedDB, 3.07 MB / 10 GB used
-
-You can use this terminal to: 
-
-    Check transactions details and start debugging.
-    Execute JavaScript scripts:
-     - Input a script directly in the command line interface 
-     - Select a Javascript file in the file explorer and then run `remix.execute()` or `remix.exeCurrent()`  in the command line interface 
-     - Right click on a JavaScript file in the file explorer and then click `Run` 
-
-The following libraries are accessible:
-
-    web3.js
-    ethers.js 
-    gpt <your question here>  
-
-Type the library name to see available commands.
-Waiting for GPT answer...
-The error occurred because the visibility specifier "public" is specified twice in the constructor declaration of the KaseiCoinCrowdsale contract. 
-
-To fix the error, remove the second "public" keyword from the constructor declaration so that it only appears once. The corrected code should be:
-
-```
-constructor(
         uint rate,
         address payable wallet,
-        KaseiCoin token
+        KaseiCoin token,
+        uint256 cap, // max amount of wei allowed to be raised
+        uint256 OpenTime, // start time
+        uint256 CloseTime // end time
         
-    ) public Crowdsale(rate, wallet, token) {
-        // constructor can stay empty
+    ) Crowdsale(rate, wallet, token)
+      CappedCrowdsale(cap)
+      TimedCrowdsale(OpenTime,CloseTime)
+    public {
     }
-```
-Waiting for GPT answer...
-rate limit exceeded, please wait 13556 ms
-Waiting for GPT answer...
-rate limit exceeded, please wait 9890 ms
-Waiting for GPT answer...
-rate limit exceeded, please wait 6030 ms
-Waiting for GPT answer...
-rate limit exceeded, please wait 2072 ms
-Waiting for GPT answer...
-The error occurred because the Solidity compiler was unable to find the file "KaseiCoin.sol" in the specified directory "./KaseiCoin.sol". 
+}
 
-To fix this error, you need to ensure that the file "KaseiCoin.sol" is in the correct directory and that the path specified in the import statement is correct. Make sure that the file is in the same directory as the contract that is importing it, or update the import statement to specify the correct path to the file.
->
-No references found for 'https'
+contract KaseiCoinCrowdsaleDeployer {
+    address public kasei_token_address;
+    address public kasei_crowdsale_address;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address payable wallet,
+        uint256 cap, // max amount of wei allowed to be raised
+        uint256 OpenTime, // start time
+        uint256 CloseTime // end time
+    )
+    public 
+    {
+        KaseiCoin token = new KaseiCoin(name, symbol, 0);
+        kasei_token_address = address(token);
+        KaseiCoinCrowdsale kasei_crowdsale = new KaseiCoinCrowdsale(1, wallet, token, cap, OpenTime, CloseTime);
+        kasei_crowdsale_address = address(kasei_crowdsale);
+        token.addMinter(kasei_crowdsale_address);
+        token.renounceMinter();
+    }
+}
